84b1bfde33e42fcb0854c39b2eb749b5e7ab8dcf
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-22 04:23:15 +0800

    added new action

--- FirstRedux/index.js
+++ FirstRedux/index.js
@@ -2,12 +2,20 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 //Action Creator: a function that returns an action (orderCake function)
 const orderCake = () => {
     return {
         type: CAKE_ORDERED,
-        quantity: 1,
+        payload: 1,
+    };
+};
+
+const restockCake = (qty = 1) => {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
     };
 };
 
@@ -24,6 +32,11 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numberOfCakes: state.numberOfCakes - 1,
             };
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes + action.payload,
+            };
         default:
             return state;
     }
@@ -35,6 +48,7 @@ console.log("Initial state", store.getState());
 const unsubscribe = store.subscribe(() => console.log("Update state ", store.getState()));
 
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 store.dispatch({
     type: CAKE_ORDERED,
 });
