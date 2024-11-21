9d6210c44dff9c3095c858821378bacdd2f3b74b
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-21 10:13:38 +0800

    implemented Action

--- FirstRedux/index.js
+++ FirstRedux/index.js
@@ -1,1 +1,7 @@
-console.log("From index.js");
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+//Action Creator: a function that returns an action (orderCake function)
+const orderCake = () => {
+    type: CAKE_ORDERED;
+    quantity: 1;
+};
