3f67671271c3e4c3c37dd806a71281d8b2e56598
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-27 04:35:17 +0800

    async thunk in redux tool kit

--- FirstReduxToolKit/index.js
+++ FirstReduxToolKit/index.js
@@ -1,20 +1,23 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions = require("./features/icecream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial state ", store.getState());
 
 const unsubscribe = store.subscribe(() => {
-    console.log(store.getState());
+    console.log("Updated state ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers());
 
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(2));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-setTimeout(unsubscribe, 100);
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(2));
+
+setTimeout(unsubscribe, 1000);
