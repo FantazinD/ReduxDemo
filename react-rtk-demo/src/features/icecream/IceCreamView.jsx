3466c689ebe57add4adc7600cc58e9d1bb646a24
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-28 02:34:42 +0800

    displayed current state to react-view

--- react-rtk-demo/src/features/icecream/IceCreamView.jsx
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const IceCreamView = () => {
+    const numOfIceCreams = useSelector((state) => {
+        console.log(state);
+        return state.iceCream.numOfIceCreams;
+    });
+
     return (
         <div>
-            <h2>Number of ice creams - </h2>
+            <h2>Number of ice creams - {numOfIceCreams}</h2>
             <button>Order ice cream</button>
             <button>Restock ice creams</button>
         </div>
